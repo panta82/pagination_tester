a5f9dafb5890af28a6fdf8e550f40ddc23c36d5e
Ivan Pantic <ivan.pantic@balkaneum.com>
2018-09-21 12:48:45 +0200

    3 variants

--- README.md
+++ README.md
@@ -14,13 +14,17 @@ Separate `SELECT *` and `SELECT COUNT(*)` queries run in parallel. The most basi
 
 Programming challenge is moderate. Need to split queries into 2 versions (one with only `WHERE`-s, other with `OFFSET`, `LIMIT` and `ORDER`). 
 
-Performance seems to be pretty stable.
-
 #### Variant 2
 
 Add a new column using a window function, so each row of the result set contains the full count. Inspired by https://stackoverflow.com/a/28888696/2405595
 
 Programming challenge is easy. Very simple to attach new row and clean up the result afterwards. Could be a challenge if you get an empty result set, and need the full count.
 
-It seems PG doesn't correctly plan for the window function. Adding `SET SESSION enable_indexscan = OFF;` to queries forces it not to use indexes. But is that a good idea? Once we force seq scan. performance is better than the Variant 1.
+Performance is very variable, see variant 3
+
+#### Variant 3
+
+Same as variant 2, with a wrapper that sets `enable_indexscan` to `OFF`. This is an attempt to improve impact of the window function, which seems to produce bad plans in some cases (index scan where full scan would be faster).
+
+Results are mixed.
 
--- bin/tester.js
+++ bin/tester.js
@@ -4,6 +4,9 @@ const libDiff = require('diff');
 const { Options } = require('../lib/options');
 const { UserStore, Criteria } = require('../lib/user_store');
 
+const VARIANTS = 3;
+const TARGET_TESTS = 5;
+
 main().catch(err => {
   console.error(err);
   process.exit(1);
@@ -26,42 +29,92 @@ function main() {
   return client
     .connect()
     .then(() => {
-      console.log('Testing...');
-      return testPage(1);
+      console.log('\nTesting the most basic query, without filters, sorted on PK...');
+      return performTest(userStore, new Criteria({ sort_field: 'id' }));
+    })
+    .then(() => {
+      console.log('\nTesting a simple filter, sort by non-unique index');
+      return performTest(
+        userStore,
+        new Criteria({ min_age: 35, sort_field: 'company', sort_direction: 'desc' })
+      );
+    })
+    .then(() => {
+      console.log('\nTesting a complex query, sorting on a unique index...');
+      return performTest(
+        userStore,
+        new Criteria({
+          filter: 'abc',
+          min_age: 50,
+          sort_field: 'ssn',
+        })
+      );
     })
     .finally(() => {
       console.log(`Done`);
       return client.end();
     });
+}
 
-  function testPage(page = 1) {
-    const criteria = new Criteria({ page });
-    let startedAt = new Date();
-    process.stdout.write(`    [Page ${page}]`);
+function performTest(userStore, criteriaTemplate) {
+  return testPage(1);
 
-    return userStore.listV1(criteria).then(result1 => {
-      const elapsed = new Date() - startedAt;
-      process.stdout.write(` V1: ${(elapsed / 1000).toFixed(2)} sec`);
+  function testPage(page) {
+    const criteria = new Criteria({ ...criteriaTemplate, page });
+    let startedAt;
+    process.stdout.write(`    [Page ${String(page).padStart(10)}]`);
 
+    return testVariant(null, 1);
+
+    function testVariant(prevResult, variant) {
       startedAt = new Date();
-      return userStore.listV2(criteria).then(result2 => {
-        const elapsed = new Date() - startedAt;
-        process.stdout.write(` V2: ${(elapsed / 1000).toFixed(2)} sec\n`);
+      return userStore['variant' + variant](criteria).then(
+        /** QueryResult */ result => {
+          const elapsed = new Date() - startedAt;
+          process.stdout.write(` \t V${variant}: ${(elapsed / 1000).toFixed(2)} sec`);
 
-        const json1 = JSON.stringify(result1, null, '  ');
-        const json2 = JSON.stringify(result2, null, '  ');
-        if (json1 !== json2) {
-          const diff = libDiff.diffLines(json1, json2);
-          console.error(`Discrepancy!`);
-          diff.forEach(part => {
-            console.error(part.added ? '+ ' : part.removed ? '- ' : '  ', part.value);
-          });
-        }
+          if (prevResult) {
+            const comp1 = { ...prevResult };
+            delete comp1.data;
+            const comp2 = { ...result };
+            delete comp2.data;
 
-        if (page < result2.total_pages) {
-          return testPage(page + 1);
+            const json1 = JSON.stringify(comp1, null, '  ');
+            const json2 = JSON.stringify(comp2, null, '  ');
+            if (json1 !== json2) {
+              console.log();
+              const diff = libDiff.diffLines(json1, json2);
+              console.error(`Discrepancy between V${variant - 1} and V${variant}!`);
+              diff.forEach(part => {
+                const prefix = part.added ? '+ ' : part.removed ? '- ' : '  ';
+                const value = part.value
+                  .split('\n')
+                  .map(str => prefix + str)
+                  .join('\n');
+                console.error(value);
+              });
+              return;
+            }
+          }
+
+          if (variant < VARIANTS) {
+            // Move to next variant
+            return testVariant(result, variant + 1);
+          }
+
+          // Done with variants
+          process.stdout.write('\n');
+          const pageSkip = Math.floor(result.total_pages / TARGET_TESTS);
+          const nextPage = result.page + pageSkip;
+          if (nextPage > result.total_pages) {
+            // We are done with pages, exit
+            return;
+          }
+
+          // Move on to next page to test
+          return testPage(nextPage);
         }
-      });
-    });
+      );
+    }
   }
 }
